bce25678aa916776c4947229a9b870b249107514
Brittik basu <brittikbasu@gmail.com>
2019-11-13 21:32:53 +0530

    version 2

--- _includes/assets/js/inline.js
+++ _includes/assets/js/inline.js
@@ -7,3 +7,22 @@ if (window.netlifyIdentity) {
     }
   });
 }
+
+/*
+var mySidebar = document.getElementsByClassName('sidebar')[0];
+mySidebar.style.maxHeight = "100px";
+console.log("my sidebar is " + mySidebar);
+var contentHeight = document.getElementsByClassName('content')[0].offsetHeight;
+*/
+/*
+function hideTabBar() {
+  if (location.pathname != "/") {
+    const hideTabs = document.querySelector("#tabBar");
+    hideTabs.style.display == "none";
+    console.log("this is" + hideTabs);
+    console.log("I'm out");
+  } else {
+    console.log("at home");
+  }
+}
+*/
